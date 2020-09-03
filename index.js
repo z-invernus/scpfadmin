0c5baa9ca6f4db0ba9030f7b597ca683567bc119
z-invernus <invernus0513@gmail.com>
2020-09-03 16:19:19 +0200

    tenth

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 const prefix = "."
 
 bot.on('ready', ()=>{
-    bot.user.setActivity(`${bot.guilds.cache.size} server.`, {type: 'WATCHING'})
+    bot.user.setActivity(`${bot.guilds.cache.size} servers.`, {type: 'WATCHING'})
     console.log('BOT Online.')
 })
 
