ba2e308298334c9247b0e7ecc634d4e3dc9bedbc
z-invernus <invernus0513@gmail.com>
2021-04-18 10:40:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,10 +75,10 @@ bot.on("message", (message) => {
     if(message.channel.name !== "executive-announcements") return;
     if(message.channel.name !== "o5-announcements") return;
     if(message.channel.name !== "foundation-announcements") return;
-    if(message.channel.name !== "department-announcements") return
-    if(message.channel.name !== "event-announcements") return
+    if(message.channel.name !== "department-announcements") return;
+    if(message.channel.name !== "event-announcements") return;
 
-    if (!message.member.hasPermission(["MENTION_EVERYONE"])) return;
+    if (!message.member.hasPermission(["MENTION_EVERYONE"])) return message.channel.send('Insufficient permissions.')
    
     let embed = new Discord.MessageEmbed()
         .setTitle("Announcement")
